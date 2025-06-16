96e19a9382d687bf2bd0fe943c6b15a6e10267aa
yosei471 <163158439+yosei471@users.noreply.github.com>
2025-06-16 23:50:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 console.log("hello");
 
+  const form = document.querySelector(".form");
+  // console.log(form);
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const data = new FormData(form);
+    console.log(data);
+    for (c of data){
+      console.log(c);
+    }
+    fetch("https://httpbin.org/post",{
+      method:"POST",
+      body: data,
+      headers:{
+        "Content-Type":"application/json",
+      },
+    })
+    .then((res) => res.json())
+    .then((res) => console.log(res));
+  });
+
+  
+
+
 "use strict";
 {
   const hamburger = document.querySelector('.hamburger');
@@ -9,5 +33,8 @@ console.log("hello");
     hamburger.classList.toggle('open');
     nav.classList.toggle('open');
   });
+
+
+
 }
 
